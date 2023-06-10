6d18c3a5e05f9d0a02d6faee70f2909d6da49755
meldren-network <meldren7@mail.ru>
2023-06-10 03:04:45 +0300

    Gradle settings reformat

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,20 @@
 plugins {
     kotlin("jvm") version "1.8.0"
-    application
 }
 
-group = "ru.meldren"
-version = "1.0"
+allprojects {
+    group = "ru.meldren"
+    version = "1.0"
+
+    repositories {
+        mavenCentral()
+    }
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
     implementation(kotlin("reflect"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
-kotlin {
-    jvmToolchain(17)
 }
